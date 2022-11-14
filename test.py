1da23e919143b4139dc58f64478b12b3d53ccb53
BuildTools <unconfigured@null.spigotmc.org>
2022-11-14 21:44:13 +0100

    test

--- test.py
+++ test.py
@@ -4,4 +4,3 @@ bahn25 = 50+(3, 75*userInput)
 bahn50 = 200+(2, 5*userInput)
 print(userInput, " Fahrten kosten ohne Bahncard", ohne,
       " mit Bahncard 25", bahn25, " und mit Bahncard 50 ", bahn50)
-d
